97e1ac983ae114ced0782ef9cdc6180c2ab04a05
Siddhesh-Devre <siddheshdevre076@gmail.com>
2025-07-01 11:31:49 +0530

    third commit

---
+++ Reverse.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class Reverse{
+    public static void main(String[] args) {
+        int[] arr ={1,2,3,4,5};
+       
+        int start = 0;
+        int end = arr.length-1;
+        int num;
+        
+         while(start<end){
+             num=arr[start];
+             arr[start] = arr[end];
+             arr[end] = num;
+             start++;
+             end--;
+         }   
+         
+        
+      
+        System.out.print(Arrays.toString(arr));
+    }
+}
