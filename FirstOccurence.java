3fab00e01dbf8de71bad6800e83459ca28443d64
Siddhesh-Devre <siddheshdevre076@gmail.com>
2025-07-01 11:51:58 +0530

    fifth commit

---
+++ FirstOccurence.java
@@ -0,0 +1,23 @@
+public class FirstOccurence {
+    public static int firstOccurrence(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            if (arr[start] == target) {
+                return start;
+            } else if (arr[end] == target) {
+                return end;
+            }
+            start++;
+            end--;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int target = 5;
+        System.out.println(firstOccurrence(arr, target));  // Output: 4
+    }
+}
